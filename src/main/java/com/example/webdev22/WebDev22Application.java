e08fc496f820e598cbf5113cb5cd2bfbc55a3acd
kongji <kongji4092@gmail.com>
2022-10-07 15:14:07 +0900

    Initial commit

---
+++ src/main/java/com/example/webdev22/WebDev22Application.java
@@ -0,0 +1,13 @@
+package com.example.webdev22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDev22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDev22Application.class, args);
+	}
+
+}
